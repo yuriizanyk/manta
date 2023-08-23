79dea78bac336ada38a895338ec1ffe6ae6a60eb
DmitriyChiroky <dimonnew777@gmail.com>
2023-08-23 17:43:00 +0300

    imgs

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -587,13 +587,13 @@ add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
 /*
 * Callback function to filter the MCE settings
 */
- 
-function my_mce_before_init_insert_formats( $init_array ) {  
- 
+
+function my_mce_before_init_insert_formats($init_array) {
+
     // Define the style_formats array
-     
-        $style_formats = array(  
-    /*
+
+    $style_formats = array(
+        /*
     * Each array child is a format with it's own settings
     * Notice that each array has title, block, classes, and wrapper arguments
     * Title is the label which will be visible in Formats menu
@@ -601,19 +601,24 @@ function my_mce_before_init_insert_formats( $init_array ) {
     * Classes allows you to define CSS classes
     * Wrapper whether or not to add a new block-level element around any selected elements
     */
-            array(  
-                'title' => 'Heading Style',  
-                'block' => 'span',  
-                'classes' => 'wcl-acf-heading-clear',
-                'wrapper' => true,
-                 
-            ),  
-        );  
-        // Insert the array, JSON ENCODED, into 'style_formats'
-        $init_array['style_formats'] = json_encode( $style_formats );  
-         
-        return $init_array;  
-       
-    } 
-    // Attach callback to 'tiny_mce_before_init' 
-    add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' ); 
+        array(
+            'title' => 'Heading Style',
+            'block' => 'span',
+            'classes' => 'wcl-acf-heading-clear',
+            'wrapper' => true,
+        ),
+
+        // array(  
+        //     'title' => 'Original Heading Style',  
+        //     'block' => 'span',  
+        //     'classes' => 'wcl-acf-original',
+        //     'wrapper' => true,
+        // ),  
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode($style_formats);
+
+    return $init_array;
+}
+// Attach callback to 'tiny_mce_before_init' 
+add_filter('tiny_mce_before_init', 'my_mce_before_init_insert_formats');
